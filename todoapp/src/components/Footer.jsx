742c8e77481aa46f8ab991d82a0cde645e327efa
Matheus Silva <matheussilvar11@gmail.com>
2024-05-08 16:53:28 -0300

    correcoes

--- todoapp/src/components/Footer.jsx
+++
@@ -1,9 +0,0 @@
-import styles from "./footer.module.css";
-export default function Footer({ completedTodos, totalTodos }) {
-  return (
-    <div className={styles.footer}>
-      <span className={styles.item}>Tarefas: {totalTodos}</span>
-      <span className={styles.item}>Tarefas Completas: {completedTodos}</span>
-    </div>
-  );
-}
